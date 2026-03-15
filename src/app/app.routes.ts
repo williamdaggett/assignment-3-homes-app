fbf25e5377fff55bc4c97e7bde6130d06fb38026
williamvdaggett@gmail.com <william.daggett@ndsu.edu>
2026-03-15 11:33:39 -0500

    Added routing to the angular application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Details } from './details/details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+        title: 'Home Page',
+    },
+    {
+        path: 'details',
+        component: Details,
+        title: 'Home Details',
+    }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import {routes} from './app/app.routes'
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, {providers: [provideRouter(routes)]})
   .catch((err) => console.error(err));
