bf64a43e32844e3e893d9f40ec8ef1067d224908
williamvdaggett@gmail.com <william.daggett@ndsu.edu>
2026-03-15 07:53:11 -0500

    Completed Creating HousingLocation Component and adding it to the Angular app

---
+++ src/app/housing-location/housing-location.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  templateUrl: './housing-location.html',
+  styleUrl: './housing-location.css',
+})
+export class HousingLocation {
+
+}
